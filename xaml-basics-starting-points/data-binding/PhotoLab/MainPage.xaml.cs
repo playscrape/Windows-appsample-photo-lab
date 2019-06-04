7fc98ae487c6ada16fe0a129b70acae141498fca
playscrape <47333457+playscrape@users.noreply.github.com>
2019-06-03 19:31:44 -0500

    Added DetermineItemSize  and DetermineItemSize

--- xaml-basics-starting-points/data-binding/PhotoLab/MainPage.xaml.cs
+++ xaml-basics-starting-points/data-binding/PhotoLab/MainPage.xaml.cs
@@ -40,7 +40,7 @@ using Windows.UI.Xaml.Navigation;
 
 namespace PhotoLab
 {
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
     {
         private ImageFileInfo persistedItem;
 
@@ -121,6 +121,62 @@ namespace PhotoLab
                     file.DisplayName, file.DisplayType);
 
                 return info; 
+            }
+        }
+
+        private void DeleteSelectedImage() =>
+            Images.Remove(ImageGridView.SelectedItem as ImageFileInfo);
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double ItemSize
+        {
+            get => _itemSize;
+            set
+            {
+                if (_itemSize != value)
+                {
+                    _itemSize = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ItemSize)));
+                }
+            }
+        }
+        private double _iemSize;
+
+        private void DetermineItemSize()
+        {
+            if (FitScreenToggle != null
+                && FitScreenToggle.IsOn == true
+                && ImageGridView != null
+                && ZoomSlider != null)
+            {
+                // The 'margins' value represents the total of the margins around the
+                // image in the grid item. 8 from the ItemTemplate root grid + 8 from
+                // the ItemContainerStyle * (Right + Left). If those values change,
+                // this value needs to be updated to match.
+                int margins = (int)this.Resources["LargeItemMarginValue"] * 4;
+                double gridWidth = ImageGridView.ActualWidth - (int)this.Resources["DesktopWindowSidePaddingValue"];
+
+                if (AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Mobile" &&
+                    UIViewSettings.GetForCurrentView().UserInteractionMode == UserInteractionMode.Touch)
+                {
+                    margins = (int)this.Resources["SmallItemMarginValue"] * 4;
+                    gridWidth = ImageGridView.ActualWidth - (int)this.Resources["MobileWindowSidePaddingValue"];
+                }
+
+                double ItemWidth = ZoomSlider.Value + margins;
+                // We need at least 1 column.
+                int columns = (int)Math.Max(gridWidth / ItemWidth, 1);
+
+                // Adjust the available grid width to account for margins around each item.
+                double adjustedGridWidth = gridWidth - (columns * margins);
+
+                ItemSize = (adjustedGridWidth / columns);
+
+            }
+            else
+            {
+                ItemSize = ZoomSlider.Value;
             }
         }
         
